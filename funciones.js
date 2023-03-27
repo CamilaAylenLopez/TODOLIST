166dc4be96603f1c210eea0ae6f74534d97bda79
CAMILA AYLEN LOPEZ <47344372@ort>
2023-03-27 10:22:28 -0300

    algo

---
+++ funciones.js
@@ -0,0 +1,13 @@
+var item;
+var lista = [""]
+
+//element.setAttribute(atributo, valor)
+
+function AñadirItem(items) {
+    var items = document.getElementById(item)
+    lista.push(items)
+    for (var i = 0; i < lista.length; i++) {
+        document.getElementById("tarea").innerHTML = lista;
+        document.createElement("tarea");
+    }
+}
