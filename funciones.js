c2ff01e87390d94508ea63c06bc6133e2812f951
CAMILA AYLEN LOPEZ <47344372@ort>
2023-03-29 08:40:38 -0300

    avance

--- funciones.js
+++ funciones.js
@@ -11,3 +11,34 @@ function AñadirItem(items) {
         document.createElement("tarea");
     }
 }
+
+
+
+const el = document.querySelector("#mainbutton");
+
+function tasklist() {
+  let item = document.getElementById('MainInput').value;
+  let text = document.createTextNode(item);
+  var checkboxmaker = document.createElement('input');
+  let newTask = document.createElement("p");
+  checkboxmaker.type = "checkbox";
+  checkboxmaker.name = "name";
+  checkboxmaker.value = "value";
+  checkboxmaker.id = "id"
+  newTask.appendChild(checkboxmaker);
+  checkboxmaker.classList = "corrected"
+  newTask.appendChild(text);
+  document.getElementById("MainUl").appendChild(newTask);
+}
+
+el.addEventListener("click", tasklist);
+
+function markdone(e) {
+  if (e.target.type == "checkbox") {
+    e.target.parentNode.classList.toggle("donete")
+  }
+}
+
+MainUl.addEventListener("click", function(e) {
+  markdone(e)
+})
