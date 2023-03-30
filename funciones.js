d4e1919135dfc1480ab254a3ba13f3b5a6e516e2
CamilaAylenLopez <camilalopezort@gmail.com>
2023-03-30 20:27:47 -0300

    diseño y clase

--- funciones.js
+++ funciones.js
@@ -1,9 +1,22 @@
+class Tarea{
+    constructor(Titulo, Descripcion, Hora){
+        this.Titulo = Titulo
+        this.Descripcion = Descripcion
+        this.Hora = Hora
+        this.Realizado = undefined
+    }
+    Hora(){
+        this.Realizado = new Date()
+        this.Tardanza = this.Realizado - this.Hora
+    }
+}
+
 var item;
 var lista = []
+const btnEnviar = document.querySelector("#mainbutton")
 
 //element.setAttribute(atributo, valor)
-
-function AñadirItem(items) {
+const AñadirItem = (items) =>{
     var items = document.getElementById(item)
     lista.push(items)
     for (var i = 0; i < lista.length; i++) {
